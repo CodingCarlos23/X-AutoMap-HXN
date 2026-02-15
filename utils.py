4d2550d81c8dc1f388fad003e765d8f2eca6ee79
Ajith Pattammattel <pattammattel@bnl.gov>
2026-02-15 03:32:12 -0500

    reduce grid step size from 20 to 10 in mosaic_overlap_scan_auto_relative function

--- utils.py
+++ utils.py
@@ -2181,7 +2181,7 @@ def mosaic_overlap_scan_auto_relative(dets = None, ylen = 100, xlen = 100, overl
 
     # 1. Define the step size for the mosaic grid
     # Since you requested 25 um steps for the grid iteration:
-    grid_step = 20
+    grid_step = 10
 
     # 2. Generate the relative step lists
     # This creates a list of positions starting at 0 up to the length
